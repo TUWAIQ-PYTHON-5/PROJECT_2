11070774ed575a85b580d1962d2144ec3fbf7be9
Sara Alotaibi <alotaibisara49@gmail.com>
2023-02-13 14:41:21 +0300

    Project 2

---
+++ PersonalWebsite/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+# Create your views here.
+
+def index(request :HttpRequest):
+    return render (request, 'main/index.html')
+
+
+def proj1(request :HttpRequest):
+    return render (request, 'main/proj1.html')
+
+def proj2(request :HttpRequest):
+    return render (request, 'main/proj2.html')
