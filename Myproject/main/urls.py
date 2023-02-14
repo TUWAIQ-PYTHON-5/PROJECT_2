433d8312d70e8915d0699c947568fbde05decda8
nada.abdullah <none.4292@gmail.com>
2023-02-14 22:49:30 +0300

    first commet,adding the project 2 !

---
+++ Myproject/main/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = "main"
+
+
+urlpatterns = [
+
+    path('', views.index_page, name="index_page"),
+    path('contact/', views.contact_me, name="contact"),
+    path('journey/', views.my_journey, name="journey"), 
+ 
+ 
+   
+
+
+   
+]
